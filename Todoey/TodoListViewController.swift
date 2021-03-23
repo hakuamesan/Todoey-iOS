d70f9305d2bc5f7a19380443c3dd0f5495127cd6
Hakuame <hakuame@gmail.com>
2021-03-23 20:49:12 +0000

    add checkmark toggle

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -30,7 +30,22 @@ class TodoListViewController: UITableViewController {
     }
     
     
-    // MARK - TableView Delegate Methods
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    
+        
+    }
     
 }
 
